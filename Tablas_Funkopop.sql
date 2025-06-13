67ab8a19587b86ce88d41adafd77ed7d2c3c0167
Sofia Laballeja <90810174+sofialaballeja21@users.noreply.github.com>
2025-06-13 19:10:39 -0300

    Update Tablas_Funkopop.sql

--- Tablas_Funkopop.sql
+++ Tablas_Funkopop.sql
@@ -33,13 +33,18 @@ CREATE TABLE peticionProducto (
     peticion VARCHAR(500) NOT NULL,
     fechaPedido   DATE DEFAULT current_date,  -- La fecha del pedido debe ser menor o iguala la fecha actual
     idusuarios SERIAL, 
-    CONSTRAINT fk_Usuario FOREIGN KEY (idusuarios) REFERENCES usuario(idusuarios)
+    idproducto SERIAL,
+    CONSTRAINT fk_Usuario FOREIGN KEY (idusuarios) REFERENCES usuario(idusuarios),
+    constraint fk_Producto foreign key (idProducto) references producto(idProducto)
 );
+
+
 
 CREATE TABLE reseniaComentario (
     idReseniaComentario SERIAL PRIMARY KEY,
-    comentario VARCHAR(100)  NOT null , --CORREGIR EN DICCIONARIO
+    comentario VARCHAR(100)  NOT NULL, 
     resenia INTEGER NOT NULL CHECK (resenia BETWEEN 1 AND 5),
+    fecharesenia date default current_date,
     idusuarios SERIAL, 
     CONSTRAINT fk_Usuario FOREIGN KEY (idusuarios) REFERENCES usuario(idusuarios)
 );
@@ -57,8 +62,8 @@ CREATE TABLE producto (
     esEspecial BOOLEAN NOT NULL,
     descripcion TEXT NOT NULL,
     brilla BOOLEAN NOT NULL,
-    precio REAL NOT null check (precio >= 0) ,
-    stock INTEGER NOT NULL check (stock >= 0),
+    precio REAL NOT NULL CHECK (precio >= 0) ,
+    stock INTEGER NOT NULL CHECK (stock >= 0),
     URL_imagen VARCHAR(500) NOT NULL,
     idColeccion SERIAL,
     CONSTRAINT fk_Coleccion FOREIGN KEY (idColeccion) REFERENCES coleccion(idColeccion)
@@ -79,27 +84,32 @@ CREATE TABLE promocion (
     idPromocion SERIAL PRIMARY KEY,
     porcentaje SERIAL NOT NULL CHECK (porcentaje BETWEEN 0 AND 100),
     fecha_inicio  DATE,
-    fecha_fin  DATE check(fecha_fin > fecha_inicio ),
+    fecha_fin  DATE CHECK (fecha_fin > fecha_inicio ),
     idProducto SERIAL,
     CONSTRAINT fk_Producto FOREIGN KEY (idProducto) REFERENCES producto(idProducto)
 );
 
-
+create type estado_factura  as enum ('pendiente de pago', 'pagado', 'cancelado', 'enviado');
+create type metodo_de_pago  as enum ('debito', 'credito', 'transferencia');
 CREATE TABLE factura (
     idFactura SERIAL PRIMARY KEY,
     pagoTotal REAL NOT null check (pagoTotal > 0),
     formaPago VARCHAR(255) NOT NULL,
     productosComprados TEXT NOT NULL,
-    impuestos REAL NOT null ,  --Preguntar si se hace un trigger o se pone el 21% directamente
+    impuestos REAL NOT null ,  
     idDescuentos SERIAL,
-    fechaEmision DATE default current_date, --agregado recien
+    fechaEmision DATE DEFAULT current_date,
+    metodoPago metodo_de_pago not null,
+    estadoFactura estado_factura not null,
     CONSTRAINT fk_Descuento FOREIGN KEY (idDescuentos) REFERENCES descuento(idDescuentos)
 );
 
 
+
 CREATE TABLE lineaFactura (
     idLineaFactura SERIAL PRIMARY KEY,
-    precio REAL NOT null check(precio > 0),
+    precioTotal REAL NOT null check(precioTotal > 0),
+    precioUnitario real not null check (precioUnitario > 0),
     cantidad INTEGER NOT null check (cantidad > 0),
     idFactura SERIAL,
     idProducto SERIAL,
@@ -108,10 +118,11 @@ CREATE TABLE lineaFactura (
     CONSTRAINT fk_Producto FOREIGN KEY (idProducto) REFERENCES producto(idProducto)
 );
 
+
 CREATE TABLE productoxCarrito (
     idProductoxCarrito SERIAL PRIMARY KEY,
-    cantidad INTEGER NOT null check(cantidad > 0),
-    precio REAL NOT null check(precio > 0),
+    cantidad INTEGER NOT NULL CHECK (cantidad > 0),
+    precio real NOT NULL CHECK (precio > 0),
     idProducto SERIAL,
     idCarrito SERIAL,
     CONSTRAINT fk_Carrito FOREIGN KEY (idCarrito) REFERENCES carrito(idCarrito),
@@ -122,7 +133,7 @@ CREATE TABLE productoxCarrito (
 
 CREATE TABLE ingresoStock (
     idStock SERIAL PRIMARY KEY,
-    cantidadIngresada INTEGER NOT null check (cantidadIngresada > 0),
+    cantidadIngresada INTEGER NOT NULL CHECK (cantidadIngresada > 0),
     idProducto SERIAL,
     CONSTRAINT fk_Producto FOREIGN KEY (idProducto) REFERENCES producto(idProducto)
 );
@@ -130,7 +141,7 @@ CREATE TABLE ingresoStock (
 
 CREATE TABLE codigoSeguimiento (
     idCodigoSeguimiento SERIAL PRIMARY KEY,
-    codigo VARCHAR(10) NOT null UNIQUE,
+    codigo VARCHAR(10) NOT NULL UNIQUE,
     idFactura SERIAL,
     CONSTRAINT fk_Factura FOREIGN KEY (idFactura) REFERENCES factura(idFactura)
 );
